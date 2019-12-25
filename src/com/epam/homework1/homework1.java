233718379ac425265c91c002853768179e92876f
anna <annkopa89@gmail.com>
2019-12-25 09:05:50 +0200

    add Homework1

---
+++ src/com/epam/homework1/homework1.java
@@ -0,0 +1,14 @@
+package com.epam.homework1;
+
+public class homework1 {
+    public static void main(String[] arg){
+        System.out.println("Hello World");
+        System.out.println("****  "+"****     "+"*     "+"*   "+"    *");
+        System.out.println("*     "+"*  *    "+"* *    "+"* *   * * ");
+        System.out.println("**    "+"****   "+"*   *  "+" *   *   * ");
+        System.out.println("*     "+"*     "+"*******  "+"*       *");
+        System.out.println("****  *    "+"*       * "+"*       *");
+
+
+    }
+}
